d595927a75fa1dfb34c309045f9c3b002cb67764
Umka Umka <vitalitsyoma@icloud.com>
2023-01-17 19:12:23 +0200

    initial

--- shop_system/order_processing/tasks.py
+++ shop_system/order_processing/tasks.py
@@ -2,10 +2,6 @@ from shop_system.celery import app
 from celery import shared_task
 from django.core.management import call_command
 
-@app.task
-def sample_task():
-    print('hello from AZOV')
-
 
 @shared_task
 def change_price_product():
